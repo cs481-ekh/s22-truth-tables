d7e37236825e25cd95837fdaf8bc3bcc6f7a85cc
josh <joshgandolfo@u.boisestate.edu>
2022-02-09 21:14:10 -0700

    add new pages, error, configure security, add chap query, student view

--- src/main/java/com/bsu/truthtables/controller/MainController.java
+++ src/main/java/com/bsu/truthtables/controller/MainController.java
@@ -26,13 +26,33 @@ public class MainController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("question" , new Question());
+        model.addAttribute("chapters", dao.getChapters());
         return "home";
     }
 
-    @PostMapping("/")
-    public String homeSubmit(@ModelAttribute Question question, Model model) {
-        model.addAttribute("q", question);
-        return "home-submit";
+    @PostMapping("/practice-problem")
+    public String practiceProblem(@ModelAttribute Question question, Model model) {
+        int numberOfInputs = parser.parseChars(question.getQuestion()).length();
+        int boxDepth = (int) Math.round(Math.pow(2,numberOfInputs));
+        String chars = parser.parseChars(question.getQuestion());
+        model.addAttribute("question", question);
+        model.addAttribute("prefilled", prefilledBox.get(String.valueOf(numberOfInputs)));
+        model.addAttribute("numberOfInputs", numberOfInputs);
+        model.addAttribute("inputChars", chars);
+        model.addAttribute("boxDepth", boxDepth);
+
+        return "practice-problem";
+    }
+    @PostMapping("/chapter-questions")
+    public String getChapterQuestions(@ModelAttribute Question question, Model model) {
+        //this is where we would grade and check stuff then load resulting page
+        return "graded";
+    }
+    @GetMapping("/chapter-questions/{chapter}")
+    public String getChapterQuestions2(@PathVariable("chapter") int chapter, Model model) {
+        model.addAttribute("chapter", chapter);
+        model.addAttribute("listOfQuestions", dao.getAllByChapter(chapter));
+        return "chapter-questions";
     }
 
     @GetMapping("/admin")
@@ -53,6 +73,6 @@ public class MainController {
         model.addAttribute("inputChars", chars);
         model.addAttribute("boxDepth", boxDepth);
         dao.add(question.getQuestion(), question.getChapter(), chars);
-        return "result";
+        return "admin-result";
     }
 }
