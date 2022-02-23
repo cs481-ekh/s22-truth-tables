1da77bed4c17c72c4d51fef78cea1170938fd60c
josh <joshgandolfo@u.boisestate.edu>
2022-02-22 21:29:14 -0700

    do some stuff

--- src/main/java/com/bsu/truthtables/parser/Parser.java
+++ src/main/java/com/bsu/truthtables/parser/Parser.java
@@ -3,7 +3,8 @@ package com.bsu.truthtables.parser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 
@@ -13,69 +14,290 @@ public class Parser {
     @Value("${operators}")
     private String operators;   //loaded from applicaiton properties, this is all the known operators
     private int rows;
+    private String stmt;
+    private String chars;
+    @Value("#{${prefilledBox}}")
+    private Map<String, String> prefilledBox;
+    private HashMap<String, String> map = null;
+
 
     public String parseChars(String question) {
-        return question.replaceAll("[" + operators + "]", "").replaceAll(",", "").replaceAll(" ", "").chars().distinct().mapToObj(c -> String.valueOf((char) c)).collect(Collectors.joining());
+        return  question.replaceAll("[" + operators + "]", "")
+                .replaceAll("\\(", "")
+                .replaceAll("\\)", "")
+                .replaceAll(" ", "")
+                .chars()
+                .distinct()
+                .mapToObj(c -> String.valueOf((char) c)).collect(Collectors.joining());
     }
 
-    public Object parseQuestion(String question) throws Exception {
-        //example string as of now = A | B, B ^ C | D. calling parse chars should return ABCD in order it was added. for example B | A | C would return BAC
+    public HashMap parseQuestion(String question) {
+        map = new HashMap<>();
+        this.stmt = question.replaceAll(" ", "");
+        this.chars = parseChars(question);
+        String values = prefilledBox.get(String.valueOf(chars.length()));
+        int i = 0;
+        while (i < values.length()) {
+            if (get(chars.charAt(i % chars.length())) == null) {
+                put(chars.charAt(i % chars.length()), String.valueOf(values.charAt(i)));  //this should map each literal to
 
-       //todo work your magic here Kyle!
-        ArrayList<ArrayList<String>> parsed = new ArrayList<>();
-
-        String lits = parseChars(question);
-        rows = (int)Math.pow(2, lits.length());
-
-        ArrayList<String> statements = new ArrayList<>();
-        ArrayList<String> values = new ArrayList<>();
-
-
-        String[] tokens = question.split(",");
-        for(String token: tokens) {
-            statements.add(token);
-            values.add(evaluateStatement(token));
+            } else {
+                put(chars.charAt(i % chars.length()), get(chars.charAt(i % chars.length())) + values.charAt(i));   //this should map each literal to
+            }
+            i++;
         }
-
-        parsed.add(statements);
-        parsed.add(values);
-        //thoughts, do we save the worked table in the database on parse each on each time pages is loaded? if we do store in db how should we save it?
-        // TTTFTTFFTTFTFFTT could be a string and if we always go width by height first then fill so TTT, FTT, FFT, etc would be the top three boxes reading left to right and down. idk
-        //def open to ideas
-
-        return parsed;
+        stmt();
+        return map;
     }
 
-    public String evaluateStatement(String statement) throws Exception{
-        String operator = "";
-        for(int i = 0; i < statement.length(); i++) {
-            String op = "";
-            op += statement.charAt(i);
-            if(operators.contains(op)) {
-                operator = op;
-                break;
-            }
+    public Object stmt() {
+        Object obj = t0();
+        if (more() && peek() == ',') {
+            eat(',');
+            Object obj2 = stmt();
+            return comma(obj, obj2);  //handle questions with a comma
         }
-        if(operator.isEmpty()) {
-            throw new Exception("Error: No operator in statement '" + statement + "'");
+        if (more() && peek() == ':') {
+            eat(':');
+            eat('.');
+            Object obj2 = stmt();
+            return therefore(obj, obj2);  //handle therefore
         }
-        String solution = "";
-        String var1 = "TTFF";
-        String var2 = "TFTF";
-        for(int i = 0; i < rows; i++) {
-            boolean v1 = var1.charAt(i) == 'T';
-            boolean v2 = var2.charAt(i) == 'T';
-            if(operator.equals("^")) {
-                solution += v1 && v2 ? "T": "F";
-            }
-            else if(operator.equals("v")) {
-                solution += v1 || v2 ? "T": "F";
-            }
-            else if(operator.equals(">")) {
-                solution += (!v1 || v2) ? "T": "F";
-            }
-        }
-        return solution;
+        return obj;
     }
 
+    public Object t0() {
+        Object obj = t1();
+        if (more() && peek() == '<' && doublePeek() == '-' && triplePeek() == '>') {
+            eat('<');
+            eat('-');
+            eat('>');
+            Object obj2 = t1();
+            return doubleArrow(obj, obj2);
+        }
+        return obj;
+    }
+
+    public Object t1() {
+        Object obj = t2();
+        if (more() && peek() == '<' && doublePeek() == '-') {
+            eat('<');
+            eat('-');
+            Object obj2 = t2();
+            return leftArrow(obj, obj2);
+        }
+        if (more() && peek() == '-' && doublePeek() == '>') {
+            eat('-');
+            eat('>');
+            Object obj2 = t2();
+            return rightArrow(obj, obj2);
+        }
+        return obj;
+    }
+
+    public Object t2() {
+        Object obj = t3();
+        while (more() && peek() == 'v') {
+            eat('v');
+            Object obj2 = t3();
+            obj = or(obj, obj2);
+        }
+        return obj;
+    }
+
+    public Object t3() {
+        Object obj = t4();
+        while (more() && peek() == '^') {
+            eat('^');
+            Object obj2 = t4();
+            obj = and(obj, obj2);
+        }
+        return obj;
+    }
+
+    public Object t4() {
+        Object obj = t5();
+        while (more() && peek() == '=') {
+            eat('=');
+            Object obj2 = t5();
+            obj = equal(obj, obj2);
+        }
+        return obj;
+    }
+
+    public Object t5() {
+        int numOfNots = 0;
+        while(peek() == '!') {
+            eat('!');
+            numOfNots++;
+        }
+        if (numOfNots != 0 && numOfNots %2 == 1) {          //idk what the char is for not so I am using ! temporarily
+            Object obj = t6();
+            return not(obj);
+        }
+        return t6();
+    }
+
+    public Object t6() {
+        if (more() && peek() == '(') {
+            eat('(');
+            Object s = stmt();
+            eat(')');
+             return paren(s);
+        }
+        return t7();
+    }
+
+    public Object t7() {
+        return next();
+    }
+
+
+    //evaluate
+    public Object paren(Object o1){
+        String name = "(" + o1 + ")";
+        put(name, get(o1));
+        map.remove(o1);
+        if(stmt.length() == 0) {
+            return name;
+        }
+        char op = next();
+        Object o2 = stmt();
+        String retval = "";
+        if(op == '^') {
+            retval = (String) and(name,o2);
+            name = name + "^" + o2;
+        }
+        if(op == 'v') {
+            retval = (String) or(name,o2);
+            name = name + "v" + o2;
+        }
+        put(name, get(retval));
+        return name;
+
+    }
+    public Object not(Object o1) {
+        String retVal = "";
+        String name = "!" + o1;
+        for (int i = 0 ; i < get(o1).length(); i++) {
+            if(get(o1).charAt(i) == 'T') {
+                retVal += "F";
+            } else{
+                retVal += "T";
+            }
+
+        }
+        map.put(name, retVal);
+        return name;
+    }
+
+    public Object equal(Object o1, Object o2) {
+        return null;
+    }
+
+    public Object and(Object o1, Object o2) {
+
+        String retVal = "";
+        String name = o1 + "^" + o2;
+        for (int i = 0 ; i < get(o1).length(); i++) {
+            if (get(o1).charAt(i) == 'T' && get(o2).charAt(i) == 'T') {
+                retVal += "T";
+            } else {
+                retVal += "F";
+            }
+        }
+        map.put(name, retVal);
+        return name;
+    }
+
+    public Object or(Object o1, Object o2) {
+        String retVal = "";
+        String name = o1 + "v" + o2;
+        for (int i = 0 ; i < get(o1).length(); i++) {
+            if (get(o1).charAt(i) == 'T' || get(o2).charAt(i) == 'T') {
+                retVal += "T";
+            } else {
+                retVal += "F";
+            }
+        }
+        map.put(name, retVal);
+        return name;
+    }
+
+    public Object rightArrow(Object o1, Object o2) {
+        return null;
+    }
+
+    public Object leftArrow(Object o1, Object o2) {
+        return null;
+    }
+
+    public Object doubleArrow(Object o1, Object o2) {
+        return null;
+    }
+
+    public Object therefore(Object o1, Object o2) {
+        return null;
+    }
+
+    public Object comma(Object o1, Object o2) {
+        return null;
+    }
+
+
+    //helper methods for parser
+    private char peek() {
+        return stmt.charAt(0);
+    }
+
+    private char doublePeek() {
+        if (stmt.length() < 1) {
+            return '0';             //return 0 since it wont ever equal a literal, else it might throw error
+        }
+        return stmt.charAt(1);
+    }
+
+    private char triplePeek() {
+        if (stmt.length() < 2) {
+            return '0';                 //return 0 since it wont ever equal a literal, else it might throw error
+        }
+        return stmt.charAt(2);
+    }
+
+    private void eat(char c) {
+        if (peek() == c)
+            this.stmt = this.stmt.substring(1);
+        else
+            throw new RuntimeException("Expected: " + c + "; got: " + peek());
+    }
+
+    private void eatWhiteSpace() {
+        if (stmt.length() > 0 && stmt.charAt(0) == ' ')
+            this.stmt = this.stmt.substring(1);
+    }
+
+    private char next() {
+        char c = peek();
+        eat(c);
+        return c;
+    }
+
+    private boolean more() {
+        return stmt.length() > 0;
+    }
+    public String get(Object o){
+        if(o instanceof String) {
+            return map.get(o);
+        } else {
+            return map.get(String.valueOf(o));
+        }
+    }
+    public String put(Object o, String val){
+        if(o instanceof String) {
+
+            return map.put(o.toString(), val);
+        } else {
+            return map.put(String.valueOf(o), val);
+        }
+    }
 }
--- src/test/java/com/bsu/truthtables/TruthtablesApplicationTests.java
+++ src/test/java/com/bsu/truthtables/TruthtablesApplicationTests.java
@@ -1,13 +1,50 @@
 package com.bsu.truthtables;
 
+import com.bsu.truthtables.parser.Parser;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
 
 @SpringBootTest
 class TruthtablesApplicationTests {
 
+	@Autowired
+	Parser parser;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void basicEval(){
+		String question1 = "A ^ B";
+		String question2 = "A v B";
+		String question3 = "! B";
+		String question4 = "A v B v C";
+		String question5 = "A ^ B ^ C";
+		String question6 = "! ! A";
+		String question7 = "! ! ! ! !A";
+		String question8 = "A^B";
+		String question9 = "(A^B)";
+		String question10 = "(A^B)vc";
+		String question11 = "(AvB) ^c";
+
+		Map q1 =parser.parseQuestion(question1);
+		Map q2 =parser.parseQuestion(question2);
+		Map q3 =parser.parseQuestion(question3);
+		Map q4 =parser.parseQuestion(question4);
+		Map q5 =parser.parseQuestion(question5);
+		Map q6 =parser.parseQuestion(question6);
+		Map q7 =parser.parseQuestion(question7);
+		Map q8 =parser.parseQuestion(question8);
+		Map q9 =parser.parseQuestion(question9);
+		Map q10 =parser.parseQuestion(question10);
+		Map q11 =parser.parseQuestion(question11);
+
+		//todo add assertions
+		System.out.println("stop");
 	}
 
 }
