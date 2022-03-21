45dc4d9c657c3bf19e1668d95555fc2d2b611327
kyle-worley <kyle@worleyclan.com>
2022-03-20 23:26:25 -0600

    Add structure for problem types

--- src/main/java/com/bsu/truthtables/domain/ParsedQuestion.java
+++ src/main/java/com/bsu/truthtables/domain/ParsedQuestion.java
@@ -8,9 +8,26 @@ import java.util.ArrayList;
 @Data
 public class ParsedQuestion {
 
-    private ArrayList<Pair<String, String>> resultList;
+    //Front end will iterate through each statement in this list in order to grab
+    //that statement's resultList
+    private ArrayList<ArrayList<Pair<String, String>>> statementList;
+
+    //List of last column values for each problem type
+    private ArrayList<String> showsNotTautology;
+    private ArrayList<String> showsEquivalent;
+    private ArrayList<String> showsConsistent;
+    private ArrayList<String> showsInvalid;
+
+    //Problem types
     private boolean argument = false;
     private boolean consistency = false;
     private boolean equivalence = false;
     private boolean logical = false;
+
+    //Final answer values
+    private boolean isTautology = false;
+    private boolean isContradiction = false;
+    private boolean isEquivalent = false;
+    private boolean isConsistent = false;
+    private boolean isValid = false;
 }
